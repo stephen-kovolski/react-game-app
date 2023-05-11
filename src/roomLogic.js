b52e61c3a2f222995d891b9df981e00e995c6ab6
stephen-kovolski <s.kovolski@gmail.com>
2023-05-10 23:02:29 -0400

    math.random to pick random rooms

---
+++ src/roomLogic.js
@@ -0,0 +1,14 @@
+let rooms = [
+            {"room number": 101, "available" : true}, 
+            {"room number": 102, "available" : true}, 
+            {"room number": 103, "available" : true}
+            ]
+let index = Math.floor((Math.random() * 3));
+let calculation = rooms[index]
+
+if (calculation >= 0 && calculation <= 2){
+    calculation == false
+}
+
+
+// export default roomLogic;
