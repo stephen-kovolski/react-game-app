775beab755fd70d004cf6acea5527e28a36326f3
Stephen Kovolski <s.kovolski@gmail.com>
2023-05-15 18:55:17 -0400

    combining components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import Welcome from './components/Welcome';
-import PickRoom from './components/PickRoom';
+// import PickRoom from './components/PickRoom';
 
 
 function App() {
   return (
     <div className="App">
       <Welcome />
-      <div className='roomButton'>
+      {/* <div className='roomButton'>
         <PickRoom room="101" />
         <PickRoom room="102" />
-        <PickRoom room="103" />
-      </div>
+        <PickRoom room="103" /> 
+      </div> */}
     </div>
   );
 }
