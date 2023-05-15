4cd500ea19bfcb99fedb7a8ac7ba3395385090c6
Stephen Kovolski <s.kovolski@gmail.com>
2023-05-15 18:26:53 -0400

    working on button functionality

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 import Welcome from './components/Welcome';
 import PickRoom from './components/PickRoom';
 
+
 function App() {
   return (
     <div className="App">
       <Welcome />
       <div className='roomButton'>
-        <PickRoom room="101"/>
-        <PickRoom room="102"/>
-        <PickRoom room="103"/>
+        <PickRoom room="101" />
+        <PickRoom room="102" />
+        <PickRoom room="103" />
       </div>
-
     </div>
   );
 }
--- src/components/PickRoom.js
+++ src/components/PickRoom.js
@@ -1,12 +1,19 @@
 import React from "react";
-import rooms from "../roomLogic";
+import { useState } from "react";
 
-function PickRoom(props){
-    return(
-        <div>           
-            <button>{props.room}</button>
-        </div>
+
+
+function PickRoom(props) {
+    const [roomAvailable, setRoomAvailable] = useState(true); // Added state to track room availability
+  
+    if (!roomAvailable) {
+      return null; // Return null to remove the button from the DOM if the room is not available
+    }
+  
+    return (
+      <div>
+        <button onClick={() => setRoomAvailable(false)}>{props.room}</button> {/* Updated to set roomAvailable to false on button click */}
+      </div>
     )
-}
-
+  }
 export default PickRoom;
